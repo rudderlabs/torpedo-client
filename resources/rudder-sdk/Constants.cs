ab1282f9c0e07a4355f545f20c4ef311a1a0f336
arnab <arnab@rudderlabs.com>
2019-09-03 12:18:27 +0530

    updated database count threshold

--- resources/rudder-sdk/Constants.cs
+++ resources/rudder-sdk/Constants.cs
@@ -5,6 +5,6 @@
         public static string BASE_URL = "https://torpedo.rudderlabs.com";
         public static int FLUSH_QUEUE_SIZE = 30;
         public static int WAIT_TIME_OUT = 10;
-        public static int DB_COUNT_THRESHOLD = 10;
+        public static int DB_COUNT_THRESHOLD = 10000;
     }
 }
