e2d8ec52c01bcdfc6b7bf649f0b2bc218b13744f
arnab <arnab@rudderlabs.com>
2019-09-04 15:12:43 +0530

    removed more logs

--- RudderAnalyticsManager.cs
+++ RudderAnalyticsManager.cs
@@ -158,6 +158,7 @@ namespace Com.TorpedoLabs.Propeller.Analytics
                 eventBuilder.SetRudderProperty(customProperties);
 
                 //invoke track method
+                rudder.Track(eventBuilder);
 
                 // GameEngine.LogError("RudderAnalyticsManager: Track: " + eventType);
             }
